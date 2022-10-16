6aede8f3c1e5fcaee45dd50b21cfbc12bcd43fe1
= <=>
2022-10-16 11:51:54 -0300

    create redirect to workspace index route

--- next.config.mjs
+++ next.config.mjs
@@ -15,4 +15,13 @@ function defineNextConfig(config) {
 export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/workspace",
+        destination: "/workspace/default",
+        permanent: false,
+      },
+    ];
+  },
 });
