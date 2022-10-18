2556cc833442c28695283aae29e5357fe1584825
= <=>
2022-10-18 00:59:29 -0300

    fix already auth route

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -73,7 +73,7 @@ export default function SignIn() {
     return <div className="loader is-loading"></div>;
 
   if (session.status === "authenticated") {
-    router.push("/dash");
+    router.push("/workspace/default");
     return <div className="loader is-loading is-danger"></div>;
   }
 
