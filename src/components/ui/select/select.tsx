65d4e9b5b0c440d0705664725bef45d64778e8e9
= <=>
2023-01-30 10:11:29 -0300

    feat: add fullwidth to select ui component

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,17 +1,34 @@
+import styled from "styled-components";
+
 type Props = {
   label: string;
   touched?: boolean | undefined;
   error?: string | undefined;
   data: Array<Record<"label" | "value" | "index", string>>;
-} & JSX.IntrinsicElements["select"];
+  fullWidth?: boolean;
+};
 
-export function Select(props: Props) {
-  const { label, touched, error, data, ...rest } = props;
+type SelectProps = Props & JSX.IntrinsicElements["select"];
+
+type ContainerProps = Omit<Props, "label" | "touched" | "error" | "data">;
+
+const SelectContainer = styled.div<ContainerProps>`
+  width: ${(props) => (props.fullWidth ? "100%" : "inherit")};
+  select {
+    width: ${(props) => (props.fullWidth ? "100%" : "inherit")};
+  }
+`;
+
+export function Select(props: SelectProps) {
+  const { label, fullWidth, touched, error, data, ...rest } = props;
   return (
     <div className="field">
       <label className="label">{label}</label>
       <div className="control">
-        <div className={`select ${touched && error ? "is-danger" : ""}`}>
+        <SelectContainer
+          className={`select ${touched && error ? "is-danger" : ""}`}
+          fullWidth={fullWidth}
+        >
           <select {...rest}>
             {data.map((item) => (
               <option key={item.index} value={item.value}>
@@ -19,7 +36,7 @@ export function Select(props: Props) {
               </option>
             ))}
           </select>
-        </div>
+        </SelectContainer>
       </div>
     </div>
   );
